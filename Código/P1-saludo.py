e7b0e51aeca1c3cac0a5fc2a98ae647abed45189
Daniela Hinojosa <sahori.hnjsa13@gmail.com>
2024-11-21 21:11:01 -0600

    Create P1-saludo.py

---
+++ Código/P1-saludo.py
@@ -0,0 +1,10 @@
+´´´Imprime un saludo ´´´
+# Fecha: 2024/11/21
+# Elaborado por: Daniela Hinojosa.
+
+# Definición de la función 
+def mensaje ():
+  print ("Bienvenidos a mi repositorio")
+
+Llamado a la función
+mensaje()
