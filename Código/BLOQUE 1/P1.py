da890a1cce75339932e84a8667836f20a386c18b
Daniela Hinojosa <sahori.hnjsa13@gmail.com>
2024-11-25 17:44:35 -0600

    Create P1.py

---
+++ Código/BLOQUE 1/P1.py
@@ -0,0 +1,6 @@
+# PROGRAMA 1. Programa que solicita tu nombre y después te immprime un saludo personalizado.
+# Fecha de elaboración:2024/10/14
+# Elaborado por: Daniela Sahori Hinojosa Meza
+
+nombre = input("Dame tu nombre: ")
+print("Hola", nombre, "¡Bienvenido a Python!")
