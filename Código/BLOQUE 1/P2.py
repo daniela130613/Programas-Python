fac8622022d77222b1668f57559d4c56a6d10d69
Daniela Hinojosa <sahori.hnjsa13@gmail.com>
2024-11-25 18:00:57 -0600

    Update P2.py

--- Código/BLOQUE 1/P2.py
+++ Código/BLOQUE 1/P2.py
@@ -1,1 +1,7 @@
+# PROGRAMA 2. Programa que solicite el nombre al usuario e 
+# imprima un mensaje personalizado con el nombre ingresado 
+# Fecha de elaboración:2024/10/14
+# Elaborado por: Daniela Sahori Hinojosa Meza
 
+nombre = input ("Nombre de usuario: ")
+print ("Hola", nombre ,"que tengas un bonito día")
