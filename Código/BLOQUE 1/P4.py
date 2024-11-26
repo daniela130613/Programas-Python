05cbc05cb8704e7de62036f24fa65318ac012df6
Daniela Hinojosa <sahori.hnjsa13@gmail.com>
2024-11-25 18:02:13 -0600

    Update P4.py

--- Código/BLOQUE 1/P4.py
+++ Código/BLOQUE 1/P4.py
@@ -1,1 +1,12 @@
+# PROGRAMA 4. Programa que comprueba el funcionamiento del siguiente programa: 
+# Fecha de elaboración:2024/10/14
+# Elaborado por: Daniela Sahori Hinojosa Meza
 
+# Esto funciona bien
+print(1 + 2)
+print(1 - 2)
+print(1 * 2)
+print("Sumando cadenas " + " está bien")
+
+# Esto NO funciona
+print("No es posible " + " restar cadenas")
