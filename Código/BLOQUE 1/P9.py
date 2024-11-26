a39fe97500dd9ea1a1060e0c88daf746b5a872f0
Daniela Hinojosa <sahori.hnjsa13@gmail.com>
2024-11-25 18:06:45 -0600

    Update P9.py

--- Código/BLOQUE 1/P9.py
+++ Código/BLOQUE 1/P9.py
@@ -1,1 +1,10 @@
+# PROGRAMA 9. Programa de asignación
+# Fecha: 2024-10-16
+# Elaborado por: Daniela Sahori Hinojosa Meza
+num = 38  # El número 38 se guarda en la variable num
+num = num + 1  # Esto incrementa el valor de num en 1, y luego guarda el resultado en num.
 
+num = 38
+print(num)  # Imprime 38
+num = num + 1
+print(str(num))  # Imprime 39 (convertido en cadena)
