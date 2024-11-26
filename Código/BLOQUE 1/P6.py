3f7ec3cf1b507aa74ea00e314655d784de39529c
Daniela Hinojosa <sahori.hnjsa13@gmail.com>
2024-11-25 18:04:45 -0600

    Update P6.py

--- Código/BLOQUE 1/P6.py
+++ Código/BLOQUE 1/P6.py
@@ -1,1 +1,9 @@
+# PROGRAMA 6. Programa que calcula el área de un triángulo dados la base y la altura por el usuario
+# Fecha: 2024-10-15
+# Elaborado por: Daniela Sahori Hinojosa Meza
 
+base=float(input("Ingresa la base:  "))
+altura=float(input("Ingresa la altura:  "))
+area=(base * altura) / 2
+ 
+print("El área del triángulo es: " + str(area))
