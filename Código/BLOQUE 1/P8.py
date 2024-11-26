8b9df3ea5f4d8f2fa6c2b1828c03ccd3c989dd3d
Daniela Hinojosa <sahori.hnjsa13@gmail.com>
2024-11-25 18:05:46 -0600

    Update P8.py

--- Código/BLOQUE 1/P8.py
+++ Código/BLOQUE 1/P8.py
@@ -1,1 +1,11 @@
+# Programa 8. Programa que calcule el número de minutos, horas y meses, dado el número de días por el usuario
+# Fecha: 2024-10-16
+# Elaborado por: Daniela Sahori Hinojosa Meza
 
+dias = float(input("Ingresa el número de días: "))
+horas = dias * 24
+minutos = horas * 60
+meses = dias / 30
+print("El total de horas es: " + str(horas))
+print("El total de minutos es: " + str(minutos))
+print("El total de meses es: " + str(meses))
