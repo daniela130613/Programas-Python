57ab34a887d646d2da05f768bc428eb62abe0ee2
Daniela Hinojosa <sahori.hnjsa13@gmail.com>
2024-11-25 18:03:07 -0600

    Update P5.py

--- Código/BLOQUE 1/P5.py
+++ Código/BLOQUE 1/P5.py
@@ -1,1 +1,14 @@
+# PROGRAMA 5. Programa que sume, reste, divida y multiplique dos números e imprima el resultado 
+# Fecha de elaboración:2024/10/14
+# Elaborado por: Daniela Sahori Hinojosa Meza
 
+num_1 = int(input("Ingresa el primer número:"))
+num_2 = int(input("Ingresa el segundo número:"))
+suma = num_1 + num_2
+resta = num_1 - num_2
+division = num_1 / num_2
+multi = num_1 * num_2
+print("El valor de la suma es: " + str(suma))
+print("El valor de la resta es: " + str(resta))
+print("El valor de la división es: " + str(division))
+print("El valor de la multiplicación es: " + str(multi))
