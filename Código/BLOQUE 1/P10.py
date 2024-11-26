40aae24a221f59dd0416625af83d6f4e70bcd1c0
Daniela Hinojosa <sahori.hnjsa13@gmail.com>
2024-11-25 18:07:18 -0600

    Update P10.py

--- Código/BLOQUE 1/P10.py
+++ Código/BLOQUE 1/P10.py
@@ -1,1 +1,15 @@
+# PROGRAMA 10. Programa para probar UNICODE
+# Fecha: 2024-10-17
+# Elaborado por: Daniela Sahori Hinojosa Meza
 
+print("Probando caracteres Unicode:")
+
+print(" \u2665")  # ♥
+print(" \u263A")  # ☺
+print(" \u2605") # ★
+print(" \u266A ") # ♪
+print(" \u2663 ") # ♣ 
+print(" \u2639")  # ☹
+print(" \u262A ") # ☪ 
+print(" \u261A ") # ☚
+print(" \u263A" * 10)
