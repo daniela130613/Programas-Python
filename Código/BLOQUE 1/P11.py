ee31e761813acecaf72591fcf3c1b9aff691e85f
Daniela Hinojosa <sahori.hnjsa13@gmail.com>
2024-11-25 18:07:58 -0600

    Update P11.py

--- Código/BLOQUE 1/P11.py
+++ Código/BLOQUE 1/P11.py
@@ -1,1 +1,43 @@
+# PROGRAMA 11. Programa para analizar los operadores de comparación
+# Fecha: 2024-10-18
+# Elaborado por: Daniela Sahori Hinojosa Meza
 
+# ==, !=, >=, <=, >,<
+
+print ("-*-*- OPERADOR IGUAL (==) -*-*-")
+print( 5 == 5 ) # TRUE
+print( 10 == 7 ) # FALSE
+print( 5.0 == 5 ) # TRUE
+
+print("\n")
+
+print ("-*-*- OPERADOR DIFERENTE o NO IGUAL (!=) -*-*-") 
+print( 5 != 5 ) # FALSE
+print( 10 != 7 ) # TRUE
+print( 5.0 != 5 ) # FALSE
+
+print("\n")
+
+print ("-*-*-OPERADOR MAYOR QUE (>)-*-*-")
+print( 10 > 5 ) # TRUE
+print( 5 > 10 ) # FALSE
+
+print("\n")
+
+print ("-*-*-OPERADOR MENOR QUE (<)-*-*-")
+print( 5 < 10 ) # TRUE
+print( 10 < 5 ) # FALSE
+
+print("\n")
+
+print ("-*-*- OPERADOR MAYOR O IGUAL QUE  (>=) -*-*-")
+print( 5 >= 5 ) # TRUE
+print( 10 >= 7 ) # TRUE
+print( 3 >= 5 ) # FALSE
+
+print("\n")
+
+print ("-*-*-OPERADOR MENOR QUE O IGUAL QUE (<=) -*-*-")
+print( 5 <= 5 ) # TRUE
+print( 10 <= 7 ) # FALSE
+print( 3 <= 5 ) # TRUE
