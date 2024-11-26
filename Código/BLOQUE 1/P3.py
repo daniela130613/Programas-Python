dfd8e0b1ae363624dcb746c7dcb48275662b4284
Daniela Hinojosa <sahori.hnjsa13@gmail.com>
2024-11-25 18:01:39 -0600

    Update P3.py

--- Código/BLOQUE 1/P3.py
+++ Código/BLOQUE 1/P3.py
@@ -1,1 +1,13 @@
+# # PROGRAMA 3. El prgrama imprimirá tres datos diferentes: un entero, 
+# un flotante y un string
+# Fecha de elaboración:2024/10/14
+# Elaborado por: Daniela Sahori Hinojosa Meza
 
+# Imprimir el número de días en una semana. Tipo de dato entero
+print(7)
+
+# Imprimir el nombre de una figura de 3 lados. Tipo de dato string
+print("Triángulo")
+
+# Imprimir a cuantos metros equivale 250 cm. Tipo de datto flotante o float
+print("0.250 metros")
