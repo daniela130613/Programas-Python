7556a27f14d06a61a02e14be82307f15bf995630
Daniela Hinojosa <sahori.hnjsa13@gmail.com>
2024-12-02 17:49:43 -0600

    Add files via upload

---
+++ Código/BLOQUE 2/P-3.py
@@ -0,0 +1,5 @@
+# Programa 3. Progrmama que comprueba el funcionamiento de la adicción o CONCATENACIÓN  de varias cadenas de texto (String).
+# Fecha de elaboración:2024/10/22
+# Elaborado por: Daniela Sahori Hinojosa Meza
+
+print("Mi " + "nombre " + "es " + "Edinguer ")
---
+++ Código/BLOQUE 2/P-6.py
@@ -0,0 +1,9 @@
+# Programa 6. Probar los comandos de NEGATIVE SUBSCRIPT INDICES
+# Fecha de elaboración:2024/10/24
+# Elaborado por: Daniela Sahori Hinojosa Meza
+
+word = "hamster"
+print(word[-1]) # r
+print(word[1:-1]) # amste
+print(word[-3:])# ter
+print(word[:3]) # ham
