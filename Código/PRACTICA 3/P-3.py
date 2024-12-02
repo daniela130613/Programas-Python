967897fb391a9fc1a6d0b35e00fe5e7d9b16b2f1
Daniela Hinojosa <sahori.hnjsa13@gmail.com>
2024-12-02 17:37:06 -0600

    Delete Código/PRACTICA 3 directory

--- Código/PRACTICA 3/P-3.py
+++
@@ -1,5 +0,0 @@
-# Programa 3. Progrmama que comprueba el funcionamiento de la adicción o CONCATENACIÓN  de varias cadenas de texto (String).
-# Fecha de elaboración:2024/10/22
-# Elaborado por: Daniela Sahori Hinojosa Meza
-
-print("Mi " + "nombre " + "es " + "Edinguer ")
--- Código/PRACTICA 3/P-4.py
+++
@@ -1,8 +0,0 @@
-# Programa 4. Programa que calcula los impuestos de un valor e imprime de la siguiente manera. 
-# Fecha de elaboración:2024/10/22
-# Elaborado por: Daniela Sahori Hinojosa Meza
-
-pesos= float(input("Ingresa la cantidad en pesos: ")) # Convierte la entrada a un número 
-impuestos= pesos * 0.30 # Calcula el 30% de impuestos
-
-print("Los impuestos son: " + str(impuestos) + "." + "\n ¡Gracias por usar nuestro sistema!")
