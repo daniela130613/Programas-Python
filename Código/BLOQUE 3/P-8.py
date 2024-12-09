4e16c846c5e357a04704dcc6f1bbb79e233f2e5b
Daniela Hinojosa <sahori.hnjsa13@gmail.com>
2024-12-09 14:40:26 -0600

    Update P-8.py

---
+++ Código/BLOQUE 3/P-8.py
@@ -0,0 +1,16 @@
+#PROGRAMA 8. 
+#Fecha de elaboración: 30/10/2024
+#Elaborado por : Daniela Sahori Hinojosa Meza
+
+print("Indexing")
+numeros = [10,20,30]
+print(numeros[2])
+print(numeros[0])
+print(numeros[1])
+
+print(numeros[-1])
+print("\nSubscripting")
+nombres= ["Chorchis" , "Choto" , "Emiliano", "Pepe el toro"]
+print(nombres [1: ])
+print(nombres [1:5 ])#marca hasta donde llegan los elementos completos
+print(nombres [-2: ])# imprimen los que continuan de derecha a izquierda
